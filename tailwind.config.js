f6cbd680cedd28d88cab4c0474480fbb1a0b3471
Anıl Şenay <anilsenay3@gmail.com>
2020-07-22 14:44:17 +0300

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  variants: {},
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
