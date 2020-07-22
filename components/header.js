b0eeda18b6378c3262a8d641cc8c65e73e6b7152
Anıl Şenay <anilsenay3@gmail.com>
2020-07-22 17:23:58 +0300

    tailwind problem fixed, header done

--- components/header.js
+++ components/header.js
@@ -1,5 +1,10 @@
 import React from "react";
 import SearchBar from "./search_bar";
+import HomeIcon from "./icons/home_icon";
+import DmIcon from "./icons/dm_icon";
+import ExploreIcon from "./icons/explore_icon";
+import ActivityIcon from "./icons/activity_icon";
+import ProfilePic from "./profile_pic";
 
 export default function Header() {
   return (
@@ -7,6 +12,13 @@ export default function Header() {
       <div className="header-container">
         <img src="../static/images/logo.png" className="header-logo" />
         <SearchBar />
+        <div className="header-icons">
+          <HomeIcon className="header-icon" />
+          <DmIcon className="header-icon" />
+          <ExploreIcon className="header-icon" />
+          <ActivityIcon className="header-icon" />
+          <ProfilePic size={22} />
+        </div>
       </div>
     </nav>
   );
--- components/search_bar.js
+++ components/search_bar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SearchBar() {
   return (
-    <div className="search-bar flex">
+    <div className="search-bar hidden md:flex">
       <input className="search-input" placeholder="Ara"></input>
     </div>
   );
