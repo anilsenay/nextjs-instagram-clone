4caf19763225ca12ad9f64d3f9d21d31810e4790
Anıl Şenay <anilsenay3@gmail.com>
2020-07-24 16:23:06 +0300

    header icons changes as instant page's location

--- components/header.js
+++ components/header.js
@@ -1,16 +1,50 @@
 import React from "react";
+import { useRouter } from "next/router";
+
 import SearchBar from "./search_bar";
 import HomeIcon from "./icons/home_icon";
 import DmIcon from "./icons/dm_icon";
 import ExploreIcon from "./icons/explore_icon";
 import ActivityIcon from "./icons/activity_icon";
+import HomeIconActive from "./icons/home_icon_active";
+import DmIconActive from "./icons/dm_icon_active";
+import ExploreIconActive from "./icons/explore_icon_active";
+import ActivityIconActive from "./icons/activity_icon_active";
 import ProfilePic from "./profile_pic";
 import Clickable from "./clickable";
+
 import { useLoginUserData } from "../hooks/global_hook";
 
 export default function Header({ user }) {
-  const loginUserData = useLoginUserData();
+  const router = useRouter();
 
+  // set icons
+  const home =
+    router.pathname === "/" ? (
+      <HomeIconActive className="header-icon" />
+    ) : (
+      <HomeIcon className="header-icon" />
+    );
+  const messages =
+    router.pathname === "/messages" ? (
+      <DmIconActive className="header-icon" />
+    ) : (
+      <DmIcon className="header-icon" />
+    );
+  const explore =
+    router.pathname === "/explore" ? (
+      <ExploreIconActive className="header-icon" />
+    ) : (
+      <ExploreIcon className="header-icon" />
+    );
+  const activity =
+    router.pathname === "/activity" ? (
+      <ActivityIconActive className="header-icon" />
+    ) : (
+      <ActivityIcon className="header-icon" />
+    );
+
+  const loginUserData = useLoginUserData();
   return (
     <nav className="navigation fixed z-20 top-0">
       <div className="header-container">
@@ -19,18 +53,10 @@ export default function Header({ user }) {
         </Clickable>
         <SearchBar />
         <div className="header-icons flex ml-auto">
-          <Clickable href="/">
-            <HomeIcon className="header-icon" />
-          </Clickable>
-          <Clickable href="/messages">
-            <DmIcon className="header-icon" />
-          </Clickable>
-          <Clickable href="/explore">
-            <ExploreIcon className="header-icon" />
-          </Clickable>
-          <Clickable href="/activity">
-            <ActivityIcon className="header-icon" />
-          </Clickable>
+          <Clickable href="/">{home}</Clickable>
+          <Clickable href="/messages">{messages}</Clickable>
+          <Clickable href="/explore">{explore}</Clickable>
+          <Clickable href="/activity">{activity}</Clickable>
           <ProfilePic
             className={
               loginUserData.username === user && "header-profile-pic-border"
